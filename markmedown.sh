4a67b4db1bf79ba5bc8718aecec9829cadee7338
Marcus Müller <marcus@hostalia.de>
2023-11-17 22:43:27 +0100

    launcher script
    Signed-off-by: Marcus Müller <marcus@hostalia.de>

---
+++ markmedown.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+venvdir="$HOME/.local/share/markmedown/venv"
+[[ -r "$venvdir/bin/activate" ]] || (
+  mkdir -p "$venvdir"
+  python3 -m venv "$venvdir"
+  . "$venvdir/bin/activate"
+  pip install 'textual>=0.11.0'
+  cp markmedown "$venvdir/bin"
+  chmod 755 "$venvdir/bin/markmedown"
+)
+. "$venvdir/bin/activate" && "$venvdir/bin/markmedown"
